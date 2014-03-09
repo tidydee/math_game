490b4d2e09e9ce3e94654264acf10207418c063e
dawidspamer <dawidspamer@gmail.com>
2014-03-09 03:48:28 +0000

    Adding 'Better Math' & 'Restart the Game' methods - still need to fix math '0' bug

--- main.rb
+++ main.rb
@@ -1,26 +1,28 @@
 require 'colorize'
 
 
-@player_1 = 3
-@player_2 = 3
+@player1_lives = 3
+@player2_lives = 3
 
 @current_player = 1
 
 def game_start
-  puts "Welcome to the ulitmate math Game."
+  puts "WELCOME TO THE ULTIMATE MATH GAME!"
   puts "Player 1: Please type your name."
   @name_1 = gets.chomp
   puts "Player 2: Please type your name."
   @name_2 = gets.chomp 
-  while @player_1 > 0 && @player_2 > 0 
+  while @player1_lives > 0 && @player2_lives > 0 
     @num1 = rand(20)
     @num2 = rand(20) 
+    math_operator_generator
     player_question
     equation_validator
-    player_turn_order
+    player_turn_order     
   end
+game_over
 end
- # Works :)
+ 
 
 def player_turn_order
   if @current_player == 1
@@ -32,66 +34,74 @@ end
 
 def subtract_life
   if @current_player == 1
-    @player_1 -= 1
-    puts "Wrong, you now have #{@player_1} lives left.".red
+    @player1_lives -= 1
+    puts "Incorrect answer, you now have #{@player1_lives} lives left.".red
   else
-    @player_2 -= 1
-    puts "Wrong, you now have #{@player_2} lives left.".red
+    @player2_lives -= 1
+    puts "Incorrect answer, you now have #{@player2_lives} lives left.".red
   end
 end
 
+def game_over
+  if @player1_lives == 0
+    puts "GAME OVER! #{@name_1} loses!"
+    puts "Do you want to play again? Y/N?"
+    play_again = gets.chomp.downcase
+      if play_again == "y"
+        game_start
+      else
+        puts "Thank you for playing!"    
+      end
+  else
+    @player2_lives == 0
+    puts "GAME OVER! #{@name_2} loses!"
+    puts "Do you want to play again? Y/N?"
+    play_again = gets.chomp.downcase
+      if play_again == "y"
+        game_start
+      else
+        puts "Thank you for playing!"    
+      end
+  end
+end
+
+
+def math_operator_generator
+  operator = ['+','-','*','/']
+  @random_operator = operator[rand(0..3)]
+end
+
 def equation_validator
+        # if eval.("#{@num1} #{@random_operator} #{@num2}") == @player_answer
+        #   puts "Correct!".green
   if @num1 + @num2 == @player_answer
     puts "Correct!".green
+  elsif @num1 - @num2 == @player_answer
+    puts "Correct!".green
+  elsif @num1 * @num2 == @player_answer
+    puts "Correct!".green
+  elsif @num1 / @num2 == @player_answer
+    puts "Correct!".green    
   else
-    puts "Incorrect!".red
     subtract_life
   end
-end # Works :)
+end 
+
+
 
 def player_question
   if @current_player == 1
-    puts "#{@name_1}: What does #{@num1} plus #{@num2} equal?"
-    @player_answer = gets.chomp.downcase.to_i
+    puts "#{@name_1}: What does #{@num1} #{@random_operator} #{@num2} equal?"
+    #@validated_equation = eval("#{@num1} #{@random_operator} #{@num2}")
+    @player_answer = gets.chomp.to_i
   else
-    puts "#{@name_2}: What does #{@num1} plus #{@num2} equal?"
-    @player_answer = gets.chomp.downcase.to_i
+    puts "#{@name_2}: What does #{@num1} #{@random_operator} #{@num2} equal?"
+    @player_answer = gets.chomp.to_i
   end
 end
 
 game_start
-  # player_question
-  # equation_validator
-  # player_turn_order
 
-
-# if equation_validator == true 
-#   player_question
-#   equation_validator
-#   player_turn_order
-# else equation_validator == false
-#   player_question
-#   equation_validator
-#   player_turn_order
-
-    
-# end
-
-
-
-# while equation_validator == true do
-#   player_question
-#   equation_validator
-#   player_turn_order
-
-  
-# end
-
-
-# while $i < $num  do
-#    puts("Inside the loop i = #$i" )
-#    $i +=1
-# end
 
 
 
